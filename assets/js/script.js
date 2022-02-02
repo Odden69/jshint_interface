9d57576c18fe9c01dc183ea0ecfee47e0a3a30f7
Odden69 <helena.joh2@gmail.com>
2022-02-02 21:21:26 +0000

    jshint

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,47 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById('submit').addEventListener('click', e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    })
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    let head = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">Ni errors reported!</div>`;
+    } else {
+        results = `<div>Total errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>at line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+    document.getElementById('resultsModalTitle').textContent = head;
+    document.getElementById('results-content').innerHTML = results;
+    resultsModal.show();
+
+}
+
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
@@ -20,8 +61,16 @@ async function getStatus(e) {
 
 function displayStatus(data) {
     document.getElementById('resultsModalTitle').textContent = 'API Key Status';
-    document.getElementById('results-content').innerHTML = 
-    `<div class='key-status'>Your key is valid until <br>${data}</div>`;
+    document.getElementById('results-content').innerHTML =
+        `<div class='key-status'>Your key is valid until <br>${data}</div>`;
 
     resultsModal.show();
 }
+
+
+const response = fetch("https://ci-jshint.herokuapp.com/api", {
+    method: "POST",
+    headers: {
+        "Authorization": API_KEY,
+    }
+})
